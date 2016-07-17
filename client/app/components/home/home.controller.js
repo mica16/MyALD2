ede38e8856e3c5683a4bdca6dd050391cd4139d7
Michael Azerhad <m.azerhad@wealcome.com>
2016-07-18 01:43:13 +0200

    add some list to fill the content

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -3,6 +3,13 @@ var HomeController = (function () {
     function HomeController() {
         this.name = 'Hello';
     }
+    HomeController.prototype.generateNumbersList = function () {
+        var ratings = [];
+        for (var i = 0; i < 500; i++) {
+            ratings.push(i);
+        }
+        return ratings;
+    };
     return HomeController;
 }());
 exports.__esModule = true;
--- client/app/components/home/home.controller.ts
+++ client/app/components/home/home.controller.ts
@@ -5,4 +5,14 @@ export default class HomeController {
     constructor() {
         this.name = 'Hello';
     }
+
+    generateNumbersList() {
+        let ratings = [];
+
+        for (let i = 0; i < 500; i++) {
+            ratings.push(i);
+        }
+
+        return ratings;
+    }
 }
