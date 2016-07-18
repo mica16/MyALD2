94604b633c65d1a068f1c6f52dfb3c99990cf8f7
Michael Azerhad <m.azerhad@wealcome.com>
2016-07-18 14:09:33 +0200

    remove some files

--- client/app/common/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require('ionic-angular/release/js/ionic.bundle');
-var sidemenu_1 = require('./sidemenu/sidemenu');
-exports.__esModule = true;
-exports["default"] = angular.module('app.common', [
-    sidemenu_1["default"]
-]).name;
-//# sourceMappingURL=common.js.map
