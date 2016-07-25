740d2f55c514978ac49749eb41bc8b04bf4ad512
Michael Azerhad <m.azerhad@wealcome.com>
2016-07-25 15:01:23 +0200

    add ionic native and some plugin

--- client/app/app.ts
+++ client/app/app.ts
@@ -3,10 +3,22 @@ import config from './app.config';
 import Common from './common/common';
 import Components from './components/components';
 import './app.scss';
+import {Splashscreen} from 'ionic-native';
+import IonicPlatformService = ionic.platform.IonicPlatformService;
 
 angular.module('app', [
     'ionic',
     'ui.router',
     Common,
     Components
-]).config(config);
+]).config(config)
+    .run(run);
+
+function run($ionicPlatform:IonicPlatformService, $timeout) {
+    $ionicPlatform.ready(function () {
+        if(ionic.Platform.isWebView())
+        $timeout(function () {
+            Splashscreen.hide();
+        }, 500);
+    });
+}
